224f9c36e7e31f16c997a7a6afce9be9f2411759
=Mohammed <=>
2022-11-16 20:43:13 +0100

    Added button + corresponding function

--- 1stPython.py
+++ 1stPython.py
@@ -1,7 +1,8 @@
 import tkinter
 from tkinter import *
 root = tkinter.Tk()
-myName = Label(root, text="Hello World")
-myName.pack()
+def myClick():
+    printmsg = Label(root, text ="Du Hurensohn!").grid(column=1)
+myName = Label(root, text="Hello World").grid(row=0, column=0)
+myButton = Button(root, text="click me to get Insulted!", command=myClick).grid(row=1,column=1)
 root.mainloop()
-print ("done")
