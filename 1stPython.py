6f1fecf98d1de2f5fd1b4f47b4a7fae24911fc56
=Mohammed <=>
2022-11-17 22:52:54 +0100

    added input field and show list of saved entries button

--- 1stPython.py
+++ 1stPython.py
@@ -1,8 +1,24 @@
-import tkinter
-from tkinter import *
-root = tkinter.Tk()
-def myClick():
-    printmsg = Label(root, text ="Du Hurensohn!").grid(column=1)
-myName = Label(root, text="Hello World").grid(row=0, column=0)
-myButton = Button(root, text="click me to get Insulted!", command=myClick).grid(row=1,column=1)
+from tkinter import*
+root = Tk()
+Greeting = Label(root, text = "Hello User, please press Next and Type in your Name").grid(row= 0, column= 0)
+NameEntry = Entry(root)
+def ShowNameInput():
+    NameEntry.grid(row=2, column=0)
+    FirstSaveClick = Button(root, text = "Save", command = SaveUsername).grid(row=3, column= 0)
+    ShowListClick = Button(root, text = "Show List of Usernames", command = printUsernames).\
+        grid(row=4, column = 0)
+FirstNextClick = Button(root, text = "Next", command = ShowNameInput).grid(row=1, column= 0)
+
+usernames = []
+def SaveUsername():
+    usernames.append(NameEntry.get())
+    def ConfirmAdded():
+        printUsername = Label(root, text = "added!").grid(row=5, column=0)
+    ConfirmAdded()
+def printUsernames():
+    Users = ""
+    for user in usernames:
+        Users += user + ", "
+    UsernamesLabel = Label(root, text="The Users are %s " % Users).grid(row = 6)
+            
 root.mainloop()
